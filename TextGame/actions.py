5c9aca2fb12c077da421148126dc2190151ee1b2
kevin2314 <kevinbrucekennedy@gmail.com>
2013-08-11 14:41:11 -0400

    first commit

---
+++ TextGame/actions.py
@@ -0,0 +1,23 @@
+
+	
+class Actions():
+	
+	def __init__(self, actions):
+		self.actions = actions
+		
+	def _actions(self, do)
+		if do in self.actions:
+			return self.actions[do]
+		else:
+			return None
+	def forge(self):
+		self._action('forge')
+		
+	
+	def buy(self):
+		self._action('forge')
+
+	def sell(self):
+		self.action('forge')
+		
+
---
+++ TextGame/inventory.py
@@ -0,0 +1,23 @@
+import json
+
+def get_inventory(id):
+	ret = None
+	with open('/home/family/Python/TextGame/inventory/1.json', 'r') as f:
+		jsontext = f.read()
+		d = json.loads(jsontext)
+		d['id'] = id
+		ret = Inventory(**d)
+	return ret
+
+class Inventory():
+	
+	def __init__(self, id=0, name='Item', description='''An empty 
+	slot''', count=[]):
+		self.id = id
+		self.name = name
+		self.description = description
+		self.count = count
+	
+	def inventory(self):
+		return self._count('inventory')
+	
