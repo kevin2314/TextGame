ca807c2fdbee57bd7d7c18c1dfdf8808adbb9181
kevin2314 <kevinbrucekennedy@gmail.com>
2013-08-31 18:36:07 -0400

    Changing Inventory from dictionary to lists inside lists

--- TextGame/inventory.py
+++ TextGame/inventory.py
@@ -2,20 +2,15 @@
 
 class Inventory(object):
 
-    def __init__(self, slots={}):
+    def __init__(self, slots=[]):
 
         self.slots = slots
 
-    def inventory(self):
-
-        self.slots = {
-                    "slot1": 'empty',
-                    "slot2": 'empty',
-                    "slot3": 'empty',
-                    "slot4": 'empty',
-                    "slot5": 'empty'
-                }
-
-if __name__ == '__main__':
-    i = Inventory()
-    i
+	def bag(self):
+	
+		slot = [
+					['Slot 1', 0]
+					['Slot 2', 0]
+					]
+		
+		
