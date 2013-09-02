e2fc61c30779cd11397dc90c349acd3ebacd05d2
kevin2314 <kevinbrucekennedy@gmail.com>
2013-09-01 22:53:00 -0400

    Code fix

--- TextGame/actions.py
+++ TextGame/actions.py
@@ -1,14 +1,8 @@
 
 
-def get_action(action):
-    action = Actions()
-    return action
-
-
 class Actions():
 
     def __init__(self, action):
-        super(Actions, self).__init__()
         self.action = action
 
     def _actions(self, cmd):
