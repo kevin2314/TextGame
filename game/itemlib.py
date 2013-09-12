13fd5bc80da02fe492ac5159e5d3a11661971848
kevin231 <kevinbrucekennedy@gmail.com>
2013-09-12 11:47:33 -0400

    changing the invnetory method

--- game/itemlib.py
+++ game/itemlib.py
@@ -12,43 +12,27 @@ class Items(object):
 
     def item_id(self, id):
 
-        self.wood = {self.item: 'wood'}
-        self.stone = {self.item: 'stone'}
-        self.dirt = {self.item: 'dirt'}
-        self.gravel = {self.item: 'gravel'}
-        self.water = {self.item: 'water'}
-        self.wooden_axe = {
-            self.item: 'Wooden Axe',
-            self.durability: 5,
-            self.gain: 3,
-            self.materials: self.wood
-        }
-        self.wooden_sword = {
-            self.item: 'Wooden Sword',
-            self.durability: 10,
-            self.gain: 0,
-            self.materials: self.wood
-        }
-        self.stone_axe = {
-            self.item: 'Stone axe',
-            self.durability: 10,
-            self.gain: 5,
-            self.materials: self.stone
-        }
-        self.stone_sword = {
-            self.item: 'stone sword',
-            self.durability: 15,
-            self.gain: 0,
-            self.materials: self.stone
-        }
+        self.wood = 'wood'
+        self.stone = 'stone'
+        self.dirt = 'dirt'
+        self.gravel = 'gravel'
+        self.water = 'water'
+        self.wooden_axe = 'wooden axe'
+        self.wooden_sword = 'wooden sword'
+        self.stone_axe = 'stone axe'
+        self.stone_sword = 'stone sword'
+
         self.id = {
-                        1: self.wood,
-                        2: self.stone,
-                        3: self.dirt,
-                        4: self.gravel,
-                        5: self.water,
-                        6: self.wooden_axe,
-                        7: self.wooden_sword,
-                        8: self.stone_axe,
-                        9: self.stone_sword
-                        }
+            1: self.wood,
+            2: self.stone,
+            3: self.dirt,
+            4: self.gravel,
+            5: self.water,
+            6: self.wooden_axe,
+            7: self.wooden_sword,
+            8: self.stone_axe,
+            9: self.stone_sword
+            }
+
+        for k, v in sorted(self.id.items()):
+            print((v))
